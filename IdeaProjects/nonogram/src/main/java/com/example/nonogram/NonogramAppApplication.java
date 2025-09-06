3674d9e21db49ef615cdd0448736e6f7e7aa2813
egor-n <e.nosko93@ya.ru>
2025-09-06 14:45:10 +0500

    Initial commit

---
+++ IdeaProjects/nonogram/src/main/java/com/example/nonogram/NonogramAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.nonogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonogramAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonogramAppApplication.class, args);
+	}
+
+}
---
+++ IdeaProjects/nonogram/src/test/java/com/example/nonogram/NonogramAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nonogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonogramAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
